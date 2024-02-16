c9bb0fcbb4c7f8cf4826941bc3bec0a547eb9734
Master Nara Lundmalm <nartek01@gmail.com>
2024-02-16 16:33:14 +0100

    Svänga vänster och höger.
    Försöker förstå mig på hur Peter har satt upp koden. Verka som att duty_cycle går från 1ms till 2ms. Vid 1ms så svänger han höger och vid 2ms så svänger han vänster vid 1.5ms så centreras han. Tror att jag har pajat servon också på den billiga bilen....

--- servo/test_pwm_servo_kod/test_pwm_servo/main.c
+++ servo/test_pwm_servo_kod/test_pwm_servo/main.c
@@ -1,16 +1,15 @@
 /*
- * main.c
- *
- * Created: 2/5/2024 12:39:31 PM
- *  Author: Peter
- */ 
+* main.c
+*
+* Created: 2/5/2024 12:39:31 PM
+*  Author: Peter
+*/
 
 #define F_CPU 16000000UL
 #include <xc.h>
 #include <util/delay.h>
 #include "p2.h"
 #include <math.h>
-
 /*Kommentar*/
 
 int main(void)
@@ -24,19 +23,51 @@ int main(void)
         off_value = PWM-låg grunden är 19000 ms för skapa en hel cykel som servot kan klara av*/
         
         uint16_t analogue_value = read_analogue_input(0);
-        uint16_t av_divided = round(analogue_value / 1.023);
-        uint16_t on_value = 1000+av_divided;
-        uint16_t off_value = 19000-av_divided;
-        
+        //uint16_t av_divided = round(analogue_value / 1.023);
+        //uint16_t on_value = 1000+av_divided;
+        //uint16_t off_value = 29000-av_divided;
+
+        void turn_left() {
+            //uint16_t analogue_value = read_analogue_input(0);
+            //uint16_t av_divided = round(analogue_value / 1.023);
+            //uint16_t on_value = 900+av_divided;
+            //uint16_t off_value = 14000-av_divided;
+
+            write_digital_output_portb(1,1);
+            _delay_ms(1);
+            write_digital_output_portb(1,0);
+            _delay_ms(1);
+        }
+
+        void turn_right() {
+            uint16_t analogue_value = read_analogue_input(0);
+            uint16_t av_divided = round(analogue_value / 1.023);
+            uint16_t on_value = 900+av_divided;
+            uint16_t off_value = 14000-av_divided;
+            write_digital_output_portb(1,1);
+            _delay_ms(2.5);
+            write_digital_output_portb(1,0);
+            _delay_ms(2.5);
+        }
+
+        void turn_straight() {
+            write_digital_output_portb(1,1);
+            _delay_ms(1.5);
+            write_digital_output_portb(1,0);
+            _delay_ms(1.5);
+        }
         
         /*PWM-signal i en for-loop med en wait emellan*/
+        turn_right();
         
-        for (uint8_t i = 0; i < 10; i++)
-        {
-            write_digital_output_portb(1,1);
-            wait(on_value);
-            write_digital_output_portb(1,0);
-            wait(off_value);
-        }
+        
+        
+        //for (uint8_t i = 0; i < 3; i++)
+        //{
+        //write_digital_output_portb(1,1);
+        //_delay_us();
+        //write_digital_output_portb(1,0);
+        //_delay_us();
+        //}
     }
 }
