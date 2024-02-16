e4a5390aacfe7ef40c42c50e1988c32d7eace57c
PeterLeisborn <petlei0802@skola.goteborg.se>
2024-02-16 12:24:51 +0100

    Kommentarer i servokod

--- servo/test_pwm_servo_kod/test_pwm_servo/main.c
+++ servo/test_pwm_servo_kod/test_pwm_servo/main.c
@@ -18,10 +18,18 @@ int main(void)
     
     while(1)
     {
+        /*analogue_value = Läser analogvärde till variabel med hjälp av Martins bibliotek från P2.
+        av_divided = Rundar av värdet från analogporten och delar med 1,023 för att få ett värde mellan 0-1000
+        on_value = PWM-hög grunden är 1 ms (delay är ändrad till us i p2.h) och adderas av av_devided
+        off_value = PWM-låg grunden är 19000 ms för skapa en hel cykel som servot kan klara av*/
+        
         uint16_t analogue_value = read_analogue_input(0);
         uint16_t av_divided = round(analogue_value / 1.023);
         uint16_t on_value = 1000+av_divided;
         uint16_t off_value = 19000-av_divided;
+        
+        
+        /*PWM-signal i en for-loop med en wait emellan*/
         
         for (uint8_t i = 0; i < 10; i++)
         {
