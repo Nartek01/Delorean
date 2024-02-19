4f24e5231bb3fcd7875f602b87d158d7cbbeb925
Master Nara Lundmalm <nartek01@gmail.com>
2024-02-19 10:19:45 +0100

    Added motor driver arduino file
    Uploaded .ino file, that turns the wheel and stop it when the sensor signal, reach a certain value.

---
+++ motor_driver.ino/motor_driver.ino.ino
@@ -0,0 +1,30 @@
+#define sensor A0
+int in1 = 3;
+int in2 = 2;
+int enA = 4;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(19200);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enA, OUTPUT);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(enA, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float volts = analogRead(sensor) * 0.00473632812;
+  int distance = 13 * pow(volts, -1);
+
+  if (distance <= 10) {
+    Serial.println(distance); // Skriv ut avstånd
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+  } else {
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(enA, HIGH);
+  }
+}
