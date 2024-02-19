880690b7050aeaac8f366228c7d911674a3390a8
PeterLeisborn <petlei0802@skola.goteborg.se>
2024-02-19 12:01:14 +0100

    Kod med filterfunktion

--- servo/test_pwm_servo_kod/test_pwm_servo/main.c
+++ servo/test_pwm_servo_kod/test_pwm_servo/main.c
@@ -15,18 +15,27 @@
 
 int main(void)
 {
+    uint16_t a = 0;
+    uint16_t b = 0;
+    uint16_t c = 0;
+    uint16_t d = 0;
+    uint16_t e = 0;
     
     while(1)
     {
-        /*analogue_value = Läser analogvärde till variabel med hjälp av Martins bibliotek från P2.
-        av_divided = Rundar av värdet från analogporten och delar med 1,023 för att få ett värde mellan 0-1000
-        on_value = PWM-hög grunden är 1 ms (delay är ändrad till us i p2.h) och adderas av av_devided
-        off_value = PWM-låg grunden är 19000 ms för skapa en hel cykel som servot kan klara av*/
+        uint8_t sensor_reading_array [] {a, b, c, d, e};
         
-        uint16_t analogue_value = read_analogue_input(0);
-        uint16_t av_divided = round(analogue_value / 1.023);
-        uint16_t on_value = 1000+av_divided;
-        uint16_t off_value = 19000-av_divided;
+        for (uint8_t i = 0; i < 4; i++)
+        {
+            sensor_reading_array[i] = read_analogue_input(0)        
+        }
+        
+        /*uint16_t analogue_value = read_analogue_input(0);*/
+       /* uint16_t av_divided = round(analogue_value / 1.023);*/
+        
+        
+        uint16_t on_value = 1000+(round(sensor_reading_array[2]*1.023));
+        uint16_t off_value = 19000-(round(sensor_reading_array[2]*1.023));
         
         
         /*PWM-signal i en for-loop med en wait emellan*/
@@ -39,4 +48,41 @@ int main(void)
             wait(off_value);
         }
     }
+}
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    
+    // One by one move boundary of
+    // unsorted subarray
+    for (i = 0; i < n - 1; i++) {
+        // Find the minimum element in
+        // unsorted array
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        if (arr[j] < arr[min_idx])
+        min_idx = j;
+        
+        // Swap the found minimum element
+        // with the first element
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    printf("%d ", arr[i]);
+    printf("\n");
 }
