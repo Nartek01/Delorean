c0012e5e30403e6e70824d5f2d53a1d88a69a397
PeterLeisborn <petlei0802@skola.goteborg.se>
2024-02-28 14:05:34 +0100

    Kod för PWM-styrning

--- servo/test_pwm_servo_kod/test_pwm_servo/main.c
+++ servo/test_pwm_servo_kod/test_pwm_servo/main.c
@@ -11,6 +11,12 @@
 #include "p2.h"
 #include <math.h>
 
+/*Funktion */
+void servo_function (int8_t turning_value)
+{
+    OCR1AL = ((0.3*turning_value)+47);
+}
+
 
 int main(void)
 {
@@ -28,16 +34,16 @@ int main(void)
     
     DDRB |= (1<<1) | (1<<2);    // Sätter utgångar D9 och D10
     OCR1AH = 0x00;              // 
-    OCR1AL = 12;                // Sätter duty cycle 
+    OCR1AL = 32;                // Sätter duty cycle 
     //OCR1BL = 150;             
-    ICR1H = 0b00000010;         // Sätter högstavärdet den räknar till 
-    ICR1L = 0b10011111;         // Sätter högstavärdet den räknar till
+    ICR1H = 0b00000010;         // Sätter högstavärdet den räknar till ICR1H = 0b00000010; + ICR1L = 0b01101111; = 50 Hz
+    ICR1L = 0b01101111;         // Sätter högstavärdet den räknar till
     TCCR1A = 0xA2;              // 
     TCCR1B = 0b00010100;        // första nibble sätter PWM-typen - sista nibble sätter prescaler (detta är 256 prescaler)
     
     while(1)
     {
-        return 0;
-        
+        /*servo_function(50);*/
     }
 }
+
