477b5f05c3cd7fc338725d197d9c468a8e976c32
Master Nara Lundmalm <nartek01@gmail.com>
2024-03-05 11:46:02 +0100

    Motorn, bromsar in mot objekt.
    Koden fungerar nästan, motorn ska egentligen back när fram-sensorn är för nära hinder. Istället så stannar den.

--- motor_driver/motor_driver.ino
+++ motor_driver/motor_driver.ino
@@ -1,33 +1,36 @@
 /** "Koll "Kooplingsschema" i Kod-Bibliotek branchen för att ser hur L298N är kopplad till arduino **/
 #define sensor A0
-int in1 = 3; // IN1 på L298N
-int in2 = 2; // IN2 på L298N
-int enA = 4; // Enable A pin
+int in1 = 3;  // IN1 på L298N
+int in2 = 2;  // IN2 på L298N
+int enA = 4;  // Enable A pin
 void setup() {
-  Serial.begin(19200); // Baudrate ser ingen skillnad på 9600 vs 19200
-  pinMode(in1, OUTPUT); // Sätter GPIO pin som kommer från IN1 från L298N till Arduino som OUTPUT
-  pinMode(in2, OUTPUT); // Sätter GPIO pin som kommer från IN2 från L298N till Arduino som OUTPUT
-  pinMode(enA, OUTPUT);  // Sätter GPIO pin som kommer från Enable A eller ENB från L298N till Arduino som OUTPUT
-  digitalWrite(enA, HIGH); // Enable A skicka 5V till L298N, för att indikera att motorn ska starta och är beredd för kommando
+  Serial.begin(19200);      // Baudrate ser ingen skillnad på 9600 vs 19200
+  pinMode(in1, OUTPUT);     // Sätter GPIO pin som kommer från IN1 från L298N till Arduino som OUTPUT
+  pinMode(in2, OUTPUT);     // Sätter GPIO pin som kommer från IN2 från L298N till Arduino som OUTPUT
+  pinMode(enA, OUTPUT);     // Sätter GPIO pin som kommer från Enable A eller ENB från L298N till Arduino som OUTPUT
+  digitalWrite(enA, HIGH);  // Enable A skicka 5V till L298N, för att indikera att motorn ska starta och är beredd för kommando
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float volts = analogRead(sensor) * 0.00473632812; // Gångra den analoga värdet med en constant så att det går att skala distans från 3 till 30
+  float volts = analogRead(sensor) * 0.00473632812;  // Gångra den analoga värdet med en constant så att det går att skala distans från 3 till 30
   int distance = 13 * pow(volts, -1);
-  if (distance < 10000) { // Motorn sluta aldrig att snurra
+  if (distance < 2000) {  // Motorn sluta aldrig att snurra
     // PWM HÖG
     digitalWrite(in1, HIGH);
-    digitalWrite(in2, HIGH); 
-    Serial.println((distance * 50)); // dumpa ut sensor värdet i millisekunder
-    delayMicroseconds(distance); // Duty-cycle
+    digitalWrite(in2, HIGH);
+    Serial.println((distance * 50));  // dumpa ut sensor värdet i millisekunder
+    delayMicroseconds(distance);      // Duty-cycle
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    delayMicroseconds((distance * 50)); // Duty-cycle
+    delayMicroseconds((distance * 50));  // Duty-cycle
     // PWM LÅG
-  } else {
-    // Stäng av motorn
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
+  } else if (distance > 400) {
+    /**
+    @TODO Bilen ska backa när "distance" variabeln är under 400 värde i längre tid.
+    Problemet är att den inte backa. Istället stannar den.
+    **/    
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, HIGH);
   }
 }
